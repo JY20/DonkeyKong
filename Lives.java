3750c54eec696b0401ea223aa40e322d3be86a72
JY20 <jimmyyan1337@gmail.com>
2019-12-17 14:38:17 -0500

    Lives image size changed

--- Lives.java
+++ Lives.java
@@ -15,5 +15,11 @@ public class Lives extends Actor
     public void act() 
     {
         // Add your action code here.
+        //if (){
+            remove();
+        //}
     }    
+    public void remove () {
+        getWorld().removeObject(this);
+    }
 }
