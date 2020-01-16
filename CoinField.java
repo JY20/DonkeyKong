1912df6528d2030bcb59f8f9e17daa820b8f8c60
JY20 <43075399+JY20@users.noreply.github.com>
2020-01-16 13:45:34 -0500

    CoinField floor problem fixed
    The floor in coinfield are located different on different size screens. So I made it the location base on the size size.

--- CoinField.java
+++ CoinField.java
@@ -20,8 +20,8 @@ public class CoinField extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(BackGround1.width, BackGround1.height, 1); 
-        int [] xs = new int [] {150, 450, 750};
-        int [] ys = new int [] {220, 400, 220};
+        int [] xs = new int [] {BackGround1.width-830, BackGround1.width-530, BackGround1.width-230};
+        int [] ys = new int [] {BackGround1.height-356, BackGround1.height-176, BackGround1.height-356};
         for(int i  = 0; i < 3; i++) {
             addObject(new Floor(), xs[i], ys[i]);
         }
@@ -31,7 +31,7 @@ public class CoinField extends World
         mario.Lives = BackGround1.mainPlayer.Lives; 
         addObject(mario, 100, BackGround1.height-30);
         addObject(new leftArrow(), 50, ys[0]-50 );
-        mario.JumpS = -20;
+        mario.JumpS = -(20);
     }
     public void act() {
         if (System.currentTimeMillis() - lT > 2000) {
